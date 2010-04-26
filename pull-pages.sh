fd847657e7437e9b3aa4e6f6cf16f637162ee400
Kyle Yetter <kcy5b@yahoo.com>
2010-04-26 04:57:53 -0400

    publishing HTML documentation work

--- pull-pages.sh
+++ pull-pages.sh
@@ -3,20 +3,29 @@
 
 set -e
 
-git checkout master doc/html
+git checkout master doc/guide/html
+git checkout master doc/api
 
 if [[ -e stylesheets ]]; then
   rm -R stylesheets
 fi
+
 if [[ -e images ]]; then
   rm -R images
 fi
+
 if [[ -e index.html ]]; then
   rm *.html
 fi
 
-mv doc/html/* .
+if [[ -d api ]]; then
+  rm -R api
+fi
+
+mv doc/guide/html/* .
+mv doc/api .
+
 git add .
-git commit -a -m "committing work on HTML guide"
+git commit -a -m "publishing HTML documentation work"
 
 git push github gh-pages
